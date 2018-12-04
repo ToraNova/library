40e8e08fe4a4dcf1f36397dcc8436ce017675b07
ToraNova <chia_jason96@live.com>
2018-12-04 18:06:09 +0800

    new java examples !

---
+++ java/ClassTester.java
@@ -0,0 +1,12 @@
+
+import MMLSutil.MMLS_get;
+
+public class ClassTester{
+
+  public static void main(String [] args){
+		System.out.println("Hello World");
+    MMLS_get mmls0 = new MMLS_get("1161302021");
+    //String sid = mmls0.getSid();
+    mmls0.printSid();
+	}
+};
