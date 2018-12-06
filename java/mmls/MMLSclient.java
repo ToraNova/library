f30d2d69c2a059c64ae87a1b5c5fb18f2c795629
ToraNova <chia_jason96@live.com>
2018-12-06 15:39:00 +0800

    working mmls autologin class

--- java/mmls/MMLSclient.java
+++ java/mmls/MMLSclient.java
@@ -30,6 +30,7 @@ public class MMLSclient{
   private static final String url_host = "mmls.mmu.edu.my";
   private static final String url_mmls_main = "https://mmls.mmu.edu.my/";
   private static final String url_mmls_check = "https://mmls.mmu.edu.my/checklogin";
+  private static final String url_mmls_home = "https://mmls.mmu.edu.my/home";
   //private static final String url_mmls_check = "http://127.0.0.1:8000";
   private String stud_id;
   private String stud_pw;
@@ -37,6 +38,7 @@ public class MMLSclient{
   //auth factors server-side
   private String session_cookie; //important
   private String login_token;
+  private Connection auth_session;
 
   public MMLSclient(String a_id,String a_pw){
     stud_id = a_id;
@@ -45,13 +47,21 @@ public class MMLSclient{
 
   public void testFunc(){
     //run testing code here.
-    login();
+    auth_session = login();
+    try{
+      System.out.println(auth_session.get());
+    }
+    catch (Exception e){
+      e.printStackTrace();
+    }
   }
 
   private Connection login(){
+    //attempts to login onto the MMLS system.
     Connection initial = Jsoup.connect(url_mmls_main);
     Connection login = Jsoup.connect(url_mmls_check);
-    login.followRedirects(false);
+    Connection auth = Jsoup.connect(url_mmls_home);
+    login.followRedirects(false); //set this to true to auto follow
 
     try{
       initial.execute();
@@ -65,7 +75,6 @@ public class MMLSclient{
       }
       //obtain the login token
       login_token = getToken(initial_response.parse());
-      login.cookie("laravel_session",session_cookie);
 
       //POST form building
       login.data("_token",login_token);
@@ -75,16 +84,16 @@ public class MMLSclient{
       //Map<String,String> login_form_headers = generateHeaders(msgLen);
       //login.headers(login_form_headers);
       login = generateHeaders(login);
+      auth = generateHeaders(auth);
+
+      Document login_response = login.post(); //execute the POST
 
       //debugging prints
       System.out.format("Laravel Session cookie: %s\nLogin token: %s\n",session_cookie,login_token);
       System.out.format("Attempting to login onto mmls with sid %s\n",stud_id);
+      System.out.format("Response title from mmls login : %s\n", login_response.title());
 
-      Document login_response = login.post(); //execute the POST
-
-      System.out.println(login_response);
-
-      return login;
+      return auth;
     }
     catch (Exception e){
       e.printStackTrace();
@@ -106,30 +115,15 @@ public class MMLSclient{
   }
 
   private Connection generateHeaders(Connection input){
-    //returns a Map interface of the required mmls login form headers
-    input.header("Host",url_host);
+    //generate the required login headers for the POST form
+    input.userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0");
     input.header("Accept-Encoding","gzip, deflate, br");
-    input.header("User-Agent","Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0");
     input.header("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
     input.header("Accept_Language","en-US,en;q=0.5");
-    input.header("Referer",url_mmls_main);
+    input.referrer(url_mmls_main);
     input.header("Content-Type","application/x-www-form-urlencoded");
-    input.header("Connection","keep-alive");
-    input.header("Cookie","laravel_session="+session_cookie);
+    input.cookie("laravel_session",session_cookie);
     input.header("Upgrade-Insecure-Requests","1");
     return input;
-
-    // out.put("Host",url_host);
-    // out.put("User-Agent",user_agt);
-    // out.put("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-    // out.put("Accept-Language","en-US,en;q=0.5");
-    // out.put("Accept-Encoding","gzip, deflate, br");
-    // out.put("Referer",url_mmls_main);
-    // out.put("Content-Type","application/x-www-form-urlencoded");
-    // out.put("Content-Length",String.valueOf(msg_length));
-    // out.put("Connection","keep-alive");
-    // out.put("Cookie","laravel_session="+session_cookie);
-    // out.put("Upgrade-Insecure-Requests","1");
-    // return out;
   }
 }
