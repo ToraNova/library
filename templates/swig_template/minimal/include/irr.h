ad9e38ce7c76512911d69ce93337aeb412cbfddd
ToraNova <chia_jason96@live.com>
2019-05-10 13:47:15 +0800

    swig_template now has 2 types, minimal and loaded_libs

---
+++ templates/swig_template/minimal/include/irr.h
@@ -0,0 +1,24 @@
+/*
+ * Irrelevant to the wrapper library
+ * This file is the test header
+ * to create a wrappr
+ */
+
+#ifndef IRR_H
+#define IRR_H
+
+/*
+ * Test function
+ */
+void test();
+
+/*
+ * Argument test
+ */
+void argtest(int arg1);
+
+//auxiliary tests
+void test0();
+
+#endif
+
