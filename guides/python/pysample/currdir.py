7594e8cb45950d2aea730fd7b7caa8b092926ebd
ToraNova <chia_jason96@live.com>
2019-05-25 22:08:32 +0800

    curdir update

--- guides/python/pysample/currdir.py
+++ guides/python/pysample/currdir.py
@@ -1,4 +1,11 @@
 import os
 
-filedir = os.path.dirname(os.path.realpath(__name__))
-print(filedir)
+
+if __name__ == "__main__":
+
+    filedir = os.path.dirname(os.path.realpath(__name__))
+    print(filedir)
+
+    onlyfiles = [f for f in os.listdir( filedir ) if \
+            os.path.isfile( os.path.join(filedir, f) )]
+    print(onlyfiles)
