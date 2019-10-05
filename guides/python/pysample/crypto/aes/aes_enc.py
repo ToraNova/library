bd902356e967c68714c0789d696100495f971bea
ToraNova <chia_jason96@live.com>
2019-10-05 09:08:27 +0800

    merge

--- guides/python/pysample/crypto/aes/aes_enc.py
+++ guides/python/pysample/crypto/aes/aes_enc.py
@@ -25,16 +25,16 @@ def gethex_bstring(s):
 def gethex_sstring(s):
 	#get the hex representation of a string (as a string)
 	return ''.join("{:02X}".format(ord(b)) for b in s )
-	
+
 
 if __name__ == "__main__":
-	
+
 	e_key = None
 	plain = None
 	allz = False
-	
+
 	AES_mode = AES.MODE_CBC
-	
+
 	#input parsing
 	if(len(sys.argv) < 3):
 		print("Please specify key and msg")
@@ -63,7 +63,7 @@ if __name__ == "__main__":
 			AES_mode = AES.MODE_CTR
 
 	#input sanitized (partially)
-	
+
 	padded = rawpad( plain , AES.block_size) 	# input padding, AES block size is fixed to 16 bytes
 	iv = os.urandom( AES.block_size ) 		# initialization vector
 
@@ -79,12 +79,12 @@ if __name__ == "__main__":
 
 	block = Ecipher.encrypt(padded)
 	cipher = iv + block 				# append the block behind the iv
-	
+
 	print("\nPure cipherblock output")
 	print("Base64	:",base64.b64encode(block).decode('utf-8'))
 	print("Hex	:",gethex_bstring(block))
 	print("Length	: {} bytes".format(len(block)))
-	
+
 	print("\nCiphertext with IV inserted:")
 	print("Base64	:",base64.b64encode(cipher).decode('utf-8'))
 	print("Hex	:",gethex_bstring(cipher))
@@ -95,7 +95,7 @@ if __name__ == "__main__":
 	# extract the iv out
 	iv = cipher[:AES.block_size]
 	cipher = cipher[AES.block_size:]
-	
+
 	if(AES_mode == AES.MODE_ECB):
 		Dcipher = AES.new( e_key, AES.MODE_ECB)
 	else:
